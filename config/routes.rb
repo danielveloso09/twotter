c80dfdb5fd8e7e4aa0b77e48d5365730cff62ad1
Daniel Veloso <daniel.veloso@xing.com>
2018-02-28 16:58:10 +0000

    add top menus

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   resources :posts
+
+  # Devise routes
   devise_for :users
+  devise_scope :user do
+    get 'sign_out', to: 'devise/sessions#destroy'
+  end
 
   root 'posts#index'
 end
