49690b0fff33e794b74dd72a3ba46e55234f0344
ana.sousa <ana.sousa@xing.com>
2018-03-01 12:10:16 +0000

    Configure external assets libs

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,5 +11,7 @@
 // about supported directives.
 //
 //= require rails-ujs
+//= require jquery
 //= require turbolinks
-//= require_tree .
+//= require bootstrap/dist/js/bootstrap.js
+//= require_tree
