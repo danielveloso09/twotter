80d70a6bc2db1d8989edb2c0ccb4b6cde8867297
Daniel Veloso <daniel.veloso@xing.com>
2018-03-01 10:18:03 +0000

    add user associations

--- app/models/following.rb
+++ app/models/following.rb
@@ -1,4 +1,5 @@
 class Following < ApplicationRecord
   belongs_to :user
-  belongs_to :followed_user, classs_name: 'User', foreign_key: 'followed_user_id'
+  belongs_to :followed_user, class_name: 'User', foreign_key: 'followed_user_id'
+  belongs_to :followee_user, class_name: 'User', foreign_key: 'user_id'
 end
